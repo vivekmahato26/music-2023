8ad9afc17634b74583780746c62e416da21b6bc5
chaitanya-cherry25 <allachaitanya1993@gmail.com>
2023-05-20 11:37:47 +0530

    first commit

---
+++ js/musicplayer.js
@@ -0,0 +1,47 @@
+var userinp = document.getElementsByClassName("input")
+var songlist = document.querySelector(".list")
+var audiotag = document.querySelector(".audio")
+var currentsongimg = document.querySelector(".currentsongimg");
+
+function mainfunction(event) {
+    userinp[0].value = event.target.value;
+  console.log(userinp[0].value);
+  data();
+}
+function data(){
+  songlist.innerHTML = "";
+  console.log(userinp[0].value);
+  fetch(`https://saavn.me/search/songs?query=${userinp[0].value}`)
+    .then(res => (res.json()))
+    .then(res => {
+      console.log(res);
+      res.data.results.map(finalresult => {
+        songlist.innerHTML += `
+<img src="${finalresult.image[2].link}" name="${finalresult.name}" class="songimageinjs" alt="${finalresult.downloadUrl[4].link}" /> `;
+        var a = document.querySelectorAll(".songimageinjs")
+        a.forEach((c) => {
+          c.addEventListener("click", (event)=> {
+            console.log(event);
+            audiotag.setAttribute("src", event.target.alt);
+            currentsongimg.src=event.target.currentSrc;
+          })
+        });
+      })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
