e0475d9b6b05f667a1b4f78074e046d8ac904f28
vivekmahato26 <vivekmahato26@gmail.com>
2023-05-20 12:13:16 +0530

    Code formatting

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -1,47 +1,31 @@
-var userinp = document.getElementsByClassName("input")
-var songlist = document.querySelector(".list")
-var audiotag = document.querySelector(".audio")
+var userinp = document.getElementsByClassName("input");
+var songlist = document.querySelector(".list");
+var audiotag = document.querySelector(".audio");
 var currentsongimg = document.querySelector(".currentsongimg");
 
 function mainfunction(event) {
-    userinp[0].value = event.target.value;
+  userinp[0].value = event.target.value;
   console.log(userinp[0].value);
   data();
 }
-function data(){
+function data() {
   songlist.innerHTML = "";
   console.log(userinp[0].value);
   fetch(`https://saavn.me/search/songs?query=${userinp[0].value}`)
-    .then(res => (res.json()))
-    .then(res => {
+    .then((res) => res.json())
+    .then((res) => {
       console.log(res);
-      res.data.results.map(finalresult => {
+      res.data.results.map((finalresult) => {
         songlist.innerHTML += `
 <img src="${finalresult.image[2].link}" name="${finalresult.name}" class="songimageinjs" alt="${finalresult.downloadUrl[4].link}" /> `;
-        var a = document.querySelectorAll(".songimageinjs")
+        var a = document.querySelectorAll(".songimageinjs");
         a.forEach((c) => {
-          c.addEventListener("click", (event)=> {
+          c.addEventListener("click", (event) => {
             console.log(event);
             audiotag.setAttribute("src", event.target.alt);
-            currentsongimg.src=event.target.currentSrc;
-          })
+            currentsongimg.src = event.target.currentSrc;
+          });
         });
-      })
-    })
+      });
+    });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
